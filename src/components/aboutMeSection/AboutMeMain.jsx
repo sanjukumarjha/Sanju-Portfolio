5749f1fc1b7a0030c8bd21c85fbec66af99fd6bc
Sanju Kumar Jha <122259192+sanjukumarjha@users.noreply.github.com>
2025-07-11 18:23:20 +0530

    Update AboutMeMain.jsx

--- src/components/aboutMeSection/AboutMeMain.jsx
+++ src/components/aboutMeSection/AboutMeMain.jsx
@@ -9,7 +9,7 @@ const AboutMeMain = () => {
       className="flex md:flex-row sm:flex-col gap-12 px-4 max-w-[1200px] mx-auto mt-[100px] justify-between items-center"
     >
       <motion.div
-        variants={fadeIn("right", 0)}
+        
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.7 }}
@@ -17,7 +17,7 @@ const AboutMeMain = () => {
         <AboutMeText />
       </motion.div>
       <motion.div
-        variants={fadeIn("left", 0)}
+        
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.7 }}
